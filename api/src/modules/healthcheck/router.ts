c6f6fdd4d1b54462b73ac2dd819bace85ba113a0
Theo Mathieu <theo.mathieu1@gmail.com>
2019-07-07 16:29:01 +0200

    wip

--- api/src/modules/healthcheck/router.ts
+++ api/src/modules/healthcheck/router.ts
@@ -1,13 +1,16 @@
 import { Router } from 'express';
 import { getHealthcheckStatus } from './dao';
 
-export const loadHealthcheckRoutes = (router: Router) => {
-  router.get('/websites/:websiteId/statuses', async (req, res, next) => {
-    try {
-      const statuses = await getHealthcheckStatus(req.params.websiteId);
-      res.send({ statuses: statuses.reverse() });
-    } catch (e) {
-      next(e);
-    }
-  });
+export const loadHealthcheckRoutes = (router: Router): void => {
+  router.get(
+    '/websites/:websiteId/statuses',
+    async (req, res, next): Promise<void> => {
+      try {
+        const statuses = await getHealthcheckStatus(req.params.websiteId);
+        res.send({ statuses: statuses.reverse() });
+      } catch (e) {
+        next(e);
+      }
+    },
+  );
 };
