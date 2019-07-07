02f0fc10937fd5102c27298d2ceb308d42517bb7
Theo Mathieu <theo.mathieu1@gmail.com>
2019-07-07 16:22:53 +0200

    wip

--- api/src/modules/healthcheck/router.ts
+++ api/src/modules/healthcheck/router.ts
@@ -1,7 +1,5 @@
 import { Router } from 'express';
 import { getHealthcheckStatus } from './dao';
-import { isLambdaFunctionExisting, createLambdaFunction, updateLambdaFunction } from '../aws/services';
-import { getAwsSettings } from '../settings/services';
 
 export const loadHealthcheckRoutes = (router: Router) => {
   router.get('/websites/:websiteId/statuses', async (req, res, next) => {
